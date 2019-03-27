9c8dd3e2d841dc5efa7d023cd617d372e4d04292
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2019-03-27 09:26:30 +0200

    map speed change direction

--- marshallers/speed-change.js
+++ marshallers/speed-change.js
@@ -2,12 +2,23 @@ const _ = require('lodash');
 const cheerio = require('cheerio');
 const config = require('../config');
 
+const Direction = {
+    UP: 'up',
+    DOWN: 'down'
+};
+
+const DIRECTIONS = {
+    'nouseva': Direction.UP,
+    'laskeva': Direction.DOWN
+};
+
 module.exports = {
     marshall: (absPos, limits) => {
 
         const id = `sc_${limits.ratakmvali.alku.ratakm}_${limits.ratakmvali.alku.etaisyys}`;
         const name = `${limits.ratakmvali.ratanumero} ${limits.ratakmvali.alku.ratakm}+${limits.ratakmvali.alku.etaisyys}`;
         const profileRef = `sppr_${limits.ratakmvali.ratanumero}_${limits.ratakmvali.alku.ratakm}_${limits.ratakmvali.alku.etaisyys}`;        
+        const dir = DIRECTIONS[limits.suunnattu] || Direction.UP;
         const max = _.max(_.map(limits.nopeusrajoitukset, 'nopeus'));
 
         const a = cheerio.load('<speedChange/>', config.cheerio);
@@ -15,7 +26,7 @@ module.exports = {
         a('speedChange').attr('name', name);
         a('speedChange').attr('pos', limits.ratakmvali.alku.etaisyys);
         a('speedChange').attr('absPos', absPos +  limits.ratakmvali.alku.etaisyys);
-        a('speedChange').attr('dir', limits.suunnattu || 'up'); // TODO map values to (up|down)
+        a('speedChange').attr('dir', dir);
         a('speedChange').attr('profileRef', profileRef);
         a('speedChange').attr('vMax', max);
 

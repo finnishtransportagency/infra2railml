72220edc24ac996b1897cce0c1d90165069d004b
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2019-06-19 10:06:33 +0300

    added operational type in OCPs

--- marshallers/operation-control-point.js
+++ marshallers/operation-control-point.js
@@ -4,10 +4,22 @@ const config = require('../config');
 
 // https://wiki.railml.org/index.php?title=IS:ocp
 
-const OcpType = {
-    OPERATIONAL_NAME: 'operationalName',
-    TRAFFIC_NAME: 'trafficName',
-    LOCAL_NAME: 'localName',
+const NameType = {
+    OPERATIONAL: 'operationalName',
+    TRAFFIC: 'trafficName',
+    LOCAL: 'localName',
+    OTHER: (val) => `other:${val}`
+};
+
+const OperationalType = {
+    STATION: 'station',
+    STOPPING_POINT: 'stoppingPoint',
+    DEPOT: 'depot',
+    CROSSOVER: 'crossover',
+    JUNCTION: 'junction',
+    BLOCK_POST: 'blockPost',
+    BLOCK_SIGNAL: 'blockSignal',
+    SIDING: 'siding',
     OTHER: (val) => `other:${val}`
 };
 
@@ -21,12 +33,16 @@ module.exports = {
         // TODO rid unnecessary array in services/station.js
         const liikennepaikka = _.first(liikennepaikanRaide.liikennepaikka);
 
+         // TODO Other types available in API?
+        const opType = OperationalType.STATION; 
+
         const $ = cheerio.load('<ocp/>', config.cheerio);
         $('ocp').attr('id', liikennepaikka.tunniste);
         $('ocp').attr('code', liikennepaikka.lyhenne);
         $('ocp').attr('name', liikennepaikka.kuljettajaAikatauluNimi);
+        $('ocp').attr('type', NameType.OPERATIONAL);
         $('ocp').attr('abbrevation', liikennepaikka.lyhenne);
-        $('ocp').attr('type', OcpType.OPERATIONAL_NAME);
+        $('ocp').append(`<propOperational operationalType="${opType}"/>`);
 
         return $.xml();        
     }

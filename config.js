7cee3d7bc60e9e16013e117dec309b4906abab0a
helguts <j.hecht88@web.de>
2021-04-23 15:44:44 +0200

    larger timeout, because 60000 hasnt been sufficient

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
     },
     http: {
         // The API is sometimes very slow..
-        timeout: 60000,
+        timeout: 300000,
         throttle: {
             // cache threshold for new fetch
             threshold: 15 * 60 * 1000
